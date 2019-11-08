a1824d0375970aa1ba97ea706cf577a231beaba8
Anurag <anurag.200698@gmail.com>
2019-11-08 13:38:30 +0530

    added static files and virtual environment configs

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/anurag/Desktop/.Personal Stuff/django/rp-portfolio/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
